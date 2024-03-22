cdfbb87101600d55fe73e732a89e77dc8959269a
laudkyle <kyleaby1@gmail.com>
2024-03-21 21:43:14 -0700

    bag of words version

---
+++ cancer_db-main/python.py
@@ -0,0 +1,14 @@
+list_one = [3,2,4,3,5,6]
+
+a,b = 0,0
+b,c = 0,0
+
+for i in range(0,len(list_one)-1):
+    for j in range(i+2,len(list_one)-1):
+        sum = list_one[i]+ list_one[j]
+        
+        temp.append()
+
+    maxes.append(max(temp))
+    
+print(maxes)
